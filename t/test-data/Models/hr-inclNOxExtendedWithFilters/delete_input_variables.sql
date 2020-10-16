21128cfabb1799ad055f285e82ead9e5d28b523f
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2020-10-16 13:58:59 +0200

    Create SQL files for variable rename/delete

---
+++ t/test-data/Models/hr-inclNOxExtendedWithFilters/delete_input_variables.sql
@@ -0,0 +1,4 @@
+DELETE FROM data_new WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_fattening_pigs_climate';
+DELETE FROM data_new WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_pigs_climate';
+DELETE FROM data_new WHERE data_var='Livestock::FatteningPigs[]::Housing::Type::exercise_place';
+DELETE FROM data_new WHERE data_var='Livestock::Pig[]::Housing::Type::exercise_place';
