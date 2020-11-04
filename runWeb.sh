01a9f0854c18552ced0876ab5e9e05afee6561e0
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2020-11-04 14:38:30 +0100

    Add loading spinner; cleanup resource path

--- runWeb.sh
+++ runWeb.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-raku -Ilib bin/agrammon.pl6 web t/test-data/agrammon.cfg.yaml t/test-data/Models/hr-inclNOxExtendedWithFilters/End.nhd
+raku -Ilib bin/agrammon.pl6 web etc/agrammon.cfg.yaml share/model/End.nhd
