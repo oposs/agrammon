cb79d6b1123ddc50d1676ef2ad0d7451a4fda8d0
Christoph Häni <christoph.haeni@bfh.ch>
2020-11-12 16:41:33 +0100

    Clean up and fix changes in DB variable

--- t/test-data/Models/hr-inclNOxExtendedWithFilters/input_variables_rename.sql
+++ t/test-data/Models/hr-inclNOxExtendedWithFilters/input_variables_rename.sql
@@ -1,18 +1,32 @@
-UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_housing_floor' WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::UNECE_category_1_mitigation_options_for_housing_systems_for_fattening_pigs';
-UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_housing_floor_LU' WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_fattening_pigs_slurry_channel';
-UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_housing_air' WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_fattening_pigs_air';
-UPDATE data_new SET data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_housing_floor' WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::UNECE_category_1_mitigation_options_for_housing_systems_for_pigs';
-UPDATE data_new SET data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_housing_floor_LU' WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_pigs_slurry_channel';
-UPDATE data_new SET data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_housing_air' WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_pigs_air';
-UPDATE data_new SET data_var='Livestock::Equides[]::Housing::CFreeFactor::free_correction_factor' WHERE data_var='Livestock::Equides[]::Housing::free_correction_factor';
-UPDATE data_new SET data_var='Livestock::RoughageConsuming[]::Housing::CFreeFactor::free_correction_factor' WHERE data_var='Livestock::RoughageConsuming[]::Housing::free_correction_factor';
-UPDATE data_new SET data_var='Livestock::SmallRuminants[]::Housing::CFreeFactor::free_correction_factor' WHERE data_var='Livestock::SmallRuminants[]::Housing::free_correction_factor';
-UPDATE data_new SET data_var='Livestock::OtherCattle[]::Housing::KArea::dimensioning_barn' WHERE data_var='Livestock::OtherCattle[]::Housing::Type::dimensioning_barn';
+# DairyCow
 UPDATE data_new SET data_var='Livestock::DairyCow[]::Housing::KArea::dimensioning_barn' WHERE data_var='Livestock::DairyCow[]::Housing::Type::dimensioning_barn';
 UPDATE data_new SET data_var='Livestock::DairyCow[]::Housing::Floor::mitigation_housing_floor' WHERE data_var='Livestock::DairyCow[]::Housing::Floor::UNECE_category_1_mitigation_options_for_housing_systems_for_dairy_cows';
-UPDATE data_new SET data_var='Livestock::DairyCow[]::Housing::Floor::mitigation_housing_floor_LU' WHERE data_var='Livestock::DairyCow[]::Housing::Floor::mitigation_options_for_housing_systems_for_dairy_cows_floor';
+UPDATE data_new SET data_var='Livestock::DairyCow[]::Housing::Floor::mitigation_housing_floor' WHERE data_var='Livestock::DairyCow[]::Housing::Floor::mitigation_options_for_housing_systems_for_dairy_cows_floor';
+UPDATE data_new SET data_var='Livestock::DairyCow[]::Yard::floor_properties_exercise_yard' WHERE data_var='Livestock::DairyCow[]::Yard::floor_properties_exercise_yard_SHL';
+UPDATE data_new SET data_var='Livestock::DairyCow[]::Yard::floor_properties_exercise_yard' WHERE data_var='Livestock::DairyCow[]::Yard::floor_properties_exercise_yard_LU';
+# OtherCattle
+UPDATE data_new SET data_var='Livestock::OtherCattle[]::Housing::KArea::dimensioning_barn' WHERE data_var='Livestock::OtherCattle[]::Housing::Type::dimensioning_barn';
 UPDATE data_new SET data_var='Livestock::OtherCattle[]::Housing::Floor::mitigation_housing_floor' WHERE data_var='Livestock::OtherCattle[]::Housing::Floor::UNECE_category_1_mitigation_options_for_housing_systems_for_other_cattle';
-UPDATE data_new SET data_var='Livestock::OtherCattle[]::Housing::Floor::mitigation_housing_floor_LU' WHERE data_var='Livestock::OtherCattle[]::Housing::Floor::mitigation_options_for_housing_systems_for_other_cattle_floor';
-UPDATE data_new SET data_var='Livestock::Equides[]::GrazingInput;grazing_days' WHERE data_var='Livestock::Equides[]::Grazing;grazing_days';
-UPDATE data_new SET data_var='Livestock::Equides[]::GrazingInput;grazing_hours' WHERE data_var='Livestock::Equides[]::Grazing;grazing_hours';
-UPDATE data_new SET data_var='Livestock::Poultry[]::Grazing;free_range' WHERE data_var='Livestock::Poultry[]::Outdoor;free_range';
+UPDATE data_new SET data_var='Livestock::OtherCattle[]::Housing::Floor::mitigation_housing_floor' WHERE data_var='Livestock::OtherCattle[]::Housing::Floor::mitigation_options_for_housing_systems_for_other_cattle_floor';
+UPDATE data_new SET data_var='Livestock::OtherCattle[]::Yard::floor_properties_exercise_yard' WHERE data_var='Livestock::OtherCattle[]::Yard::floor_properties_exercise_yard_SHL';
+UPDATE data_new SET data_var='Livestock::OtherCattle[]::Yard::floor_properties_exercise_yard' WHERE data_var='Livestock::OtherCattle[]::Yard::floor_properties_exercise_yard_LU';
+# FatteningPigs
+UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_housing_floor' WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::UNECE_category_1_mitigation_options_for_housing_systems_for_fattening_pigs';
+UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_housing_floor' WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_fattening_pigs_slurry_channel';
+UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_housing_air' WHERE data_var='Livestock::FatteningPigs[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_fattening_pigs_air';
+# Pig
+UPDATE data_new SET data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_housing_floor' WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::UNECE_category_1_mitigation_options_for_housing_systems_for_pigs';
+UPDATE data_new SET data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_housing_floor' WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_pigs_slurry_channel';
+UPDATE data_new SET data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_housing_air' WHERE data_var='Livestock::Pig[]::Housing::MitigationOptions::mitigation_options_for_housing_systems_for_pigs_air';
+# Equides
+UPDATE data_new SET data_var='Livestock::Equides[]::Housing::CFreeFactor::free_correction_factor' WHERE data_var='Livestock::Equides[]::Housing::free_correction_factor';
+UPDATE data_new SET data_var='Livestock::Equides[]::GrazingInput::grazing_days' WHERE data_var='Livestock::Equides[]::Grazing::grazing_days';
+UPDATE data_new SET data_var='Livestock::Equides[]::GrazingInput::grazing_hours' WHERE data_var='Livestock::Equides[]::Grazing::grazing_hours';
+UPDATE data_new SET data_var='Livestock::Equides[]::Yard::floor_properties_exercise_yard' WHERE data_var='Livestock::Equides[]::Yard::floor_properties_exercise_yard_SHL';
+UPDATE data_new SET data_var='Livestock::Equides[]::Yard::floor_properties_exercise_yard' WHERE data_var='Livestock::Equides[]::Yard::floor_properties_exercise_yard_LU';
+# SmallRuminants
+UPDATE data_new SET data_var='Livestock::SmallRuminants[]::Housing::CFreeFactor::free_correction_factor' WHERE data_var='Livestock::SmallRuminants[]::Housing::free_correction_factor';
+# RoughageConsuming
+UPDATE data_new SET data_var='Livestock::RoughageConsuming[]::Housing::CFreeFactor::free_correction_factor' WHERE data_var='Livestock::RoughageConsuming[]::Housing::free_correction_factor';
+#Poultry
+UPDATE data_new SET data_var='Livestock::Poultry[]::Grazing::free_range' WHERE data_var='Livestock::Poultry[]::Outdoor::free_range';
