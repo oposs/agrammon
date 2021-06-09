0d24aa40acf86b8e21c8937d5892d4701b0823e4
Christoph Häni <christoph.haeni@bfh.ch>
2021-06-09 18:13:02 +0200

    Rename input variable fattening_pigs to animals

--- t/test-data/Models/hr-inclNOxExtendedWithFilters/input_variables_rename_postdep.sql
+++ t/test-data/Models/hr-inclNOxExtendedWithFilters/input_variables_rename_postdep.sql
@@ -1,2 +1,4 @@
 # DairyCow
 UPDATE data_new SET data_var='Livestock::DairyCow[]::Excretion::animals' WHERE data_var='Livestock::DairyCow[]::Excretion::dairy_cows';
+# FatteningPigs
+UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Excretion::animals' WHERE data_var='Livestock::FatteningPigs[]::Excretion::fattening_pigs';
