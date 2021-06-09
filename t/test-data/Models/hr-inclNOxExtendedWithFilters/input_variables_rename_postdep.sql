5af9cb77ac816c28e71a45bad94877a3df8a8a13
Christoph Häni <christoph.haeni@bfh.ch>
2021-06-09 21:33:07 +0200

    Move input vars from Yard and Grazing to Outdoor.nhd

--- t/test-data/Models/hr-inclNOxExtendedWithFilters/input_variables_rename_postdep.sql
+++ t/test-data/Models/hr-inclNOxExtendedWithFilters/input_variables_rename_postdep.sql
@@ -4,3 +4,11 @@ UPDATE data_new SET data_var='Livestock::DairyCow[]::Excretion::animals' WHERE d
 UPDATE data_new SET data_var='Livestock::FatteningPigs[]::Excretion::animals' WHERE data_var='Livestock::FatteningPigs[]::Excretion::fattening_pigs';
 # Pig
 UPDATE data_new SET data_var='Livestock::Pig[]::Excretion::animals' WHERE data_var='Livestock::Pig[]::Excretion::pigs';
+# Equides
+UPDATE data_new SET data_var='Livestock::Equides[]::Outdoor::grazing_hours' WHERE data_var='Livestock::Equides[]::GrazingInput::grazing_hours';
+UPDATE data_new SET data_var='Livestock::Equides[]::Outdoor::grazing_days' WHERE data_var='Livestock::Equides[]::GrazingInput::grazing_days';
+UPDATE data_new SET data_var='Livestock::Equides[]::Outdoor::yard_hours' WHERE data_var='Livestock::Equides[]::Yard::yard_hours';
+UPDATE data_new SET data_var='Livestock::Equides[]::Outdoor::yard_days' WHERE data_var='Livestock::Equides[]::Yard::yard_days';
+UPDATE data_new SET data_var='Livestock::Equides[]::Outdoor::floor_properties_exercise_yard' WHERE data_var='Livestock::Equides[]::Yard::floor_properties_exercise_yard';
+# ?if !Kantonal_LU:
+UPDATE data_new SET data_var='Livestock::Equides[]::Outdoor::free_correction_factor' WHERE data_var='Livestock::Equides[]::Yard::free_correction_factor';
