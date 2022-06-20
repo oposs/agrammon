6a920821045447b4f454a20536c4fb2414d3ad04
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2022-06-20 18:48:25 +0200

    Add docu and test script/data

--- runExcelTest.sh
+++ runExcelTest.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-raku -Ilib bin/agrammon.pl6 --export-filename=text.xlsx --format=excel \
+raku -Ilib bin/agrammon.pl6 --cfg-file=etc/agrammon-excel-test.cfg.yaml \
+    --export-filename=text.xlsx --format=excel \
     --include-filters --include-all-filters --report-selected=1 \
     run \
     version6/End.nhd t/test-data/Inputs/HR_2015_cmdline.csv
